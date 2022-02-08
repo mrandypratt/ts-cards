5c65550ea58f1e372be1f23a3b2fdb91ce34f92d
mrandypratt <mrandypratt@gmail.com>
2022-02-07 22:17:54 -0600

    Migrated PlayingCard to TS

--- src/components/Error.jsx
+++
@@ -1,9 +0,0 @@
-import Alert from '@mui/material/Alert'
-
-export default function Error(props) {
-  return (
-    <Alert severity="error">
-      <strong>Error: </strong>{ props.message }
-    </Alert>
-  );
-}
