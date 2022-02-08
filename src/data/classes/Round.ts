64845517be73091b3593a0b19a0f6a3b486cdb55
mrandypratt <mrandypratt@gmail.com>
2022-02-07 19:18:40 -0600

    Migrated Round

---
+++ src/data/classes/Round.ts
@@ -0,0 +1,70 @@
+import { Player } from "./Player";
+import { PromptCard } from "./PromptCard";
+import { ResponseCard } from "./ResponseCard";
+
+type RoundProps = {
+  players: Player[];
+  judge: Player;
+  promptCard: PromptCard;
+}
+
+type SelectedCardType = {
+  [player: string]: ResponseCard | null;
+}
+
+export class Round {
+  players: Player[];
+  judge: Player;
+  promptCard: PromptCard;
+  selectedCards: SelectedCardType;
+  winningCard: ResponseCard | null;
+  winner: Player | null;
+
+  constructor({ players, judge, promptCard }: RoundProps) {
+    // this.players = game.getNonJudgePlayers();
+    this.players = players;
+    // this.judge = game.getJudgePlayer();
+    this.judge = judge;
+    // this.promptCard = game.promptCards.pop();
+    this.promptCard = promptCard;
+    this.selectedCards = {};
+    players.forEach(player => {
+      this.selectedCards[player.name] = null;
+    });
+    this.winningCard = null;
+    this.winner = null;
+  }
+
+  selectCard(name: string, card: ResponseCard): void {
+    this.selectedCards[name] = card;
+  }
+
+  hasPlayerSelected(name: string) {
+    return !!this.selectedCards[name];
+  }
+
+  getSelection(name: string): ResponseCard | null {
+    return this.selectedCards[name];
+  }
+
+  isCardSelected(name: string, card: ResponseCard): boolean {
+    return this.selectedCards[name] === card;
+  }
+
+  allSelectionsMade(): boolean {
+    return this.players.length === Object.keys(this.selectedCards).length;
+  }
+
+  removePlayedCards(): void {
+    this.players.forEach(player => {
+      const card = this.getSelection(player.name);
+      if (card !== null) {
+        player.cards.splice(player.cards.indexOf(card), 1);
+      }
+    })
+  }
+
+  isWinner(): boolean {
+    return this.winner !== null;
+  }
+}
