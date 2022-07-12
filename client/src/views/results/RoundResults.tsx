ff56fb5bf7ee1d5e8a93bd1f38a428e9012f4d1e
mrandypratt <mrandypratt@gmail.com>
2022-07-11 22:03:01 -0500

    Centered Results Table

--- client/src/views/results/RoundResults.tsx
+++ client/src/views/results/RoundResults.tsx
@@ -57,7 +57,7 @@ export const RoundResults = ({ game, setGame, socket, sessionId }: ViewPropsType
 
         </div>
   
-        <div style={{textAlign: "center", marginTop: 10}}>
+        <div style={{textAlign: "center", marginTop: 10, display: "flex", width: "100%", justifyContent: "center"}}>
           
           <ResultsTable
             game={game}
