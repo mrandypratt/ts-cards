9b795a4917e8fe201ac97f522e41ef13760bee00
mrandypratt <mrandypratt@gmail.com>
2022-10-03 21:41:40 -0500

    Removed keys and switched to dotenv

--- server/keys/mongoURI.ts
+++
@@ -1,9 +0,0 @@
-let mongoURI: string;
-
-if (process.env.NODE_ENV === 'production') {
-  mongoURI = require('./prod');
-} else {
-  mongoURI = require('./dev');
-}
-
-export { mongoURI }
--- server/keys/prod.ts
+++
@@ -1,4 +0,0 @@
-// Need to set Mongo URI on EC2
-module.exports = {
-  mongoURI: process.env.MONGO_URI || "",
-};
