330313ee53c3becea61889ccb95f67362b3fe632
mrandypratt <mrandypratt@gmail.com>
2022-10-09 21:01:13 -0500

    Added Database Connection to Server

--- client/src/views/info/Feedback.tsx
+++ client/src/views/info/Feedback.tsx
@@ -1,20 +1,13 @@
 import { Box, TextField } from "@mui/material";
 import Container from "@mui/material/Container";
 import { useState } from "react";
-import { GettingStartedButton, ReturnHomeButton, SubmitButton } from "../../components/Buttons/Submit";
+import { ReturnHomeButton, SubmitButton } from "../../components/Buttons/Submit";
 import { EVENTS } from "../../data/constants/EVENTS";
 import { VIEWS } from "../../data/constants/VIEWS";
 import { ViewPropsType } from "../../data/types/ViewPropsType";
 
-interface FeedbackData {
-  name: string;
-  email: string;
-  feedback: string;
-}
-
 export const Feedback = ({game, setGame, socket, sessionId}: ViewPropsType): JSX.Element => {
   const [ isLoading, setIsLoading ] = useState(false);
-  const [ err, setErr ] = useState('');
 
   // Form Data State
   const [ name, setName ] = useState("");
@@ -41,7 +34,7 @@ export const Feedback = ({game, setGame, socket, sessionId}: ViewPropsType): JSX
     setIsLoading(true);
 
     const baseURL = process.env.REACT_APP_STAGE === "prod" ? "52.20.228.225:8787" : "http://localhost:8787";
-    const APIURL = baseURL + "/api";
+    const APIURL = baseURL + "/api/feedback";
 
     try {
       const response = await fetch(APIURL, {
@@ -63,13 +56,13 @@ export const Feedback = ({game, setGame, socket, sessionId}: ViewPropsType): JSX
 
       const result = await response.json();
 
-      console.log('result is: ', JSON.stringify(result, null, 4));
+      console.log(result);
 
     } catch (e) {
-      setErr("Error somehow")
-      console.log(e)
+      console.error(e)
     } finally {
       setIsLoading(false);
+      socket.emit(EVENTS.client.updateView, VIEWS.home);
     }
   };
     
@@ -80,52 +73,57 @@ export const Feedback = ({game, setGame, socket, sessionId}: ViewPropsType): JSX
 
       <hr></hr>
 
-      <p>{"We love feedback."}</p>
-      <p>{"Hearing from you helps us prioritize new features and bug fixes to create a fun experience for our users"}</p>
-
-      <Box style={{marginBottom: 10}}>
-        <TextField 
-          id="standard-basic"
-          label="Name"
-          variant="outlined"
-          onChange={updateName}
-        />
-      </Box>
-
-      <Box style={{marginBottom: 10}}>
-        <TextField 
-          id="standard-basic"
-          label="Email"
-          variant="outlined"
-          onChange={updateEmail}
-        />
-      </Box>
-
-      <Box style={{marginBottom: 10}}>
-        <TextField 
-          multiline={true}
-          minRows={5}
-          id="standard-basic"
-          label="Feedback"
-          variant="outlined"
-          onChange={updateFeedback}
-          fullWidth={true}
-          helperText={"What can we improve? The more details the better :)"}
-        />
-      </Box>
-
-      <div>
-        
+      { !isLoading &&
         <div>
-          <SubmitButton 
-            text={"Submit Feedback"}
-            onClick={submitFeedback}
-            type="button"
-            disabled={false}
-          />
-        </div>
+          <p>{"We love feedback."}</p>
+          <p>{"Hearing from you helps us prioritize new features and bug fixes to create a fun experience for our users"}</p>
 
-      </div> 
+          <Box style={{marginBottom: 10}}>
+            <TextField 
+              id="standard-basic"
+              label="Name"
+              variant="outlined"
+              onChange={updateName}
+            />
+          </Box>
+
+          <Box style={{marginBottom: 10}}>
+            <TextField 
+              id="standard-basic"
+              label="Email"
+              variant="outlined"
+              onChange={updateEmail}
+            />
+          </Box>
+
+          <Box style={{marginBottom: 10}}>
+            <TextField 
+              multiline={true}
+              minRows={5}
+              id="standard-basic"
+              label="Feedback"
+              variant="outlined"
+              onChange={updateFeedback}
+              fullWidth={true}
+              helperText={"What can we improve? The more details the better :)"}
+            />
+          </Box>
+
+          <div>
+              <SubmitButton 
+                text={"Submit Feedback"}
+                onClick={submitFeedback}
+                type="button"
+                disabled={false}
+              />
+          </div>  
+        </div>
+      }
+
+      { isLoading && 
+        <h3>Submitting Feedback...</h3>
+      }
+
 
       <ReturnHomeButton
         text={"Return Home"}
