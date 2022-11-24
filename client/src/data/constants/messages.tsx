638f263b817e6439e98bea22641339631bcb2f39
mrandypratt <mrandypratt@gmail.com>
2022-11-23 22:54:23 -0600

    updated home button and info page

--- client/src/data/constants/messages.tsx
+++ client/src/data/constants/messages.tsx
@@ -1,7 +1,7 @@
 export const MESSAGES = {
 
   gettingStarted: {
-    about: "Cards with Friends is a Prompt/Response game inspired by Cards Against Humanity.\n\nCwF is designed for friend groups to play whether or not they are in the same room or they have a deck of cards.\n\n",
+    about: "Cards with Friends is an online clone of Cards Against Humanity.\n\nCwF is designed for friend groups to play whether or not they are in the same room or they have a deck of cards.\n\n",
     creatingGames: "Each game is set up by a host player clicking \"New Game\" to start a lobby. Then they are given a Lobby ID to share with up to 11 guests.\n\nOnce a guest receives the Lobby ID, they can visit the website and click \"Join Lobby\" where they can enter their Lobby ID to join the game.\n\nOnce a lobby has at least 3 total participants, the host can start the game.",
     gameplay: "Every player is dealt 5 response cards. Participants take turns being the Judge, while all the other players try to play the best response to a given prompt using the cards in their hand. The game will tell you who the Judge is, so be sure to keep their sense of humor in mind!\n\nAt the end of the Turn, the Judge will select their favorite card from all submitted cards, which have been shuffled for anonymity. The player who submitted the winning card gets a point, and then the next turn will yield a new judge.\n\nOnce a player reaches 5 points, they are declared the winner!"
   },
--- client/src/views/info/GettingStarted.tsx
+++ client/src/views/info/GettingStarted.tsx
@@ -1,4 +1,3 @@
-import { MESSAGES } from "../../data/constants/messages";
 import { EVENTS } from "../../data/constants/EVENTS";
 import { VIEWS } from "../../data/constants/VIEWS";
 import { ViewPropsType } from "../../data/types/ViewPropsType";
@@ -10,28 +9,41 @@ export const HowToPlay = ({game, setGame, socket, sessionId}: ViewPropsType): JS
   }
 
   return (
-    <div style={{ textAlign: "center" }}>
-
-      <h2>About the Game</h2>
-
-      <p>{MESSAGES.gettingStarted.about}</p>
+    <div className="info-container" style={{ textAlign: "center" }}>
+      <h1 style={{margin: "auto"}}>About the Game</h1>
 
       <hr></hr>
 
-      <h2>Creating Games</h2>
+      <div style={{border: "1px solid black", maxWidth: "90%", marginLeft: "5%"}}>
 
-      <p>{MESSAGES.gettingStarted.creatingGames}</p>
+        <h2 style={{paddingTop: "7px", margin: "auto"}}>Game Modes</h2>
 
-      <hr></hr>
+        <hr></hr>
 
-      <h2>Gameplay</h2>
+        <h3 style={{margin: "auto"}}>Multi-Player</h3>
 
-      <p>{MESSAGES.gettingStarted.gameplay}</p>
+        <p style={{marginTop: "5px"}}>Play with your friends. One friend will create the game first, then share the Lobby ID for the rest to join. Multi-player currently supports a minimum of 3 participants.</p>
+
+        <h3 style={{margin: "auto"}}>Single-Player (Beta)</h3>
+
+        <p style={{marginTop: "5px"}}>The single-player is set up to mock an online experience with randomly generated bot players. It is great to demonstrate the gameplay style, so maybe you'll invite friends :)</p>
+      
+      </div>
+
+      <h2 style={{paddingTop: "8px", margin: "auto"}}>Gameplay</h2>
+
+      <p style={{marginTop: "5px"}}>Winner is the first to 3 rounds. Currently, due to limited content, player hands are limited to 3 cards at a time. The rules are otherwise consistent with Cards Against Humanity. </p>
       
       <hr></hr>
 
+      <h2 style={{margin: "auto"}}>Content & Development</h2>
+
+      <p style={{marginTop: "5px"}}>{"All content was created by the devleoper, <APDev/>.  If you are interested in creating content or collaborating, please leave feedback with your email or follow the website link on the homepage footer."}</p>
+
       <div onClick={returnHome} style={{marginTop: 10}}>
+
         <p style={{color: "blue", textDecoration: "underline", cursor: "pointer"}}>Back to Home</p>
+      
       </div>
 
     </div>
